98e09faa4ebac165ecb2357ce2cd48ce294ccaa2
SebastianMP21 <sebas_slark123@outlook.com>
2023-08-28 12:05:58 -0500

    Pokemon dinámicos con useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import Texto from "./components/Texto"
-import Contador from "./components/Contador";
-import ListaNombres from "./components/ListaNombres"
+//import Texto from "./components/Texto";
+//import Contador from "./components/Contador";
+//import ListaNombres from "./components/ListaNombres";
+import Pokemones from "./components/Pokemones";
 
 const App = () => {
   return <div className="container">
     {/* <Texto name="Sebastian" apellido="Marquez" edad={23}/>
     <Texto name="Miguel" apellido="Alarcón" edad={25}/>
     <Contador/> */}
-    <ListaNombres/>
+    {/*<ListaNombres/>*/}
+    <Pokemones />
   </div>
 
 }
--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -6,9 +6,10 @@ const Contador = () => {
 
     return <div>
         <p>Valor actual del contador:  {contador} veces</p>
-        <button onClick={() => setContador( contador + 1 )}> Aumentar</button>
         <button onClick={() => setContador( contador - 1 )}>Disminuir</button>
         <button onClick={() => setContador(0)}> Reestablecer</button>
+        <button onClick={() => setContador( contador + 1 )}> Aumentar</button>
+        
     </div>
 }
 
